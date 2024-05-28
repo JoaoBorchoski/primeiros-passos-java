3233d42af7135a341930aee7fb7d9f5c7b5c8604
JoaoBorchoski <joaoborchoskidev@gmail.com>
2024-05-28 16:46:01 -0300

    Feat: create crud spring

--- src/main/java/com/primeiro/primeiro_app_springboot/PacienteController.java
+++ src/main/java/com/primeiro/primeiro_app_springboot/PacienteController.java
@@ -1,12 +1,10 @@
 package com.primeiro.primeiro_app_springboot;
 
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.UUID;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/paciente")
@@ -18,9 +16,33 @@ public class PacienteController {
         return pacientes;
     }
 
+    @GetMapping("/{pacienteId}")
+    public Optional<PacienteModel> getPacienteById(@PathVariable UUID pacienteId) {
+        return pacientes.stream().filter(p -> p.getId().equals(pacienteId)).findFirst();
+    }
+
     @PostMapping
     public PacienteModel createPaciente(@RequestBody PacienteModel payload) {
         pacientes.add(payload);
         return payload;
     }
+
+    @PutMapping("/{pacienteId}")
+    public Optional<PacienteModel> updatePaciente(@PathVariable UUID pacienteId, @RequestBody PacienteModel payload) {
+        Optional<PacienteModel> paciente = pacientes.stream().filter(p -> p.getId().equals(pacienteId)).findFirst();
+        paciente.ifPresent(p -> {
+            p.setNome(payload.getNome());
+            p.setIdade(payload.getIdade());
+            p.setEndereco(payload.getEndereco());
+        });
+        return paciente;
+    }
+
+    @DeleteMapping("/{pacienteId}")
+    public void deletePaciente(@PathVariable UUID pacienteId) {
+        Optional<PacienteModel> paciente = pacientes.stream().filter(p -> p.getId().equals(pacienteId)).findFirst();
+        paciente.ifPresent(p -> pacientes.remove(p));
+        return;
+    }
+
 }
